91a29bf38d42246a0a8e55b2f40dbad32a12547b
firja10 <fairuzfirjatullah3@gmail.com>
2023-04-10 07:53:08 +0700

    Update

--- projek_ahmad_db.sql
+++ projek_ahmad_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2023 at 01:51 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Waktu pembuatan: 10 Apr 2023 pada 02.52
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `activity`
+-- Struktur dari tabel `activity`
 --
 
 CREATE TABLE `activity` (
@@ -40,7 +40,7 @@ CREATE TABLE `activity` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bahan_baku`
+-- Struktur dari tabel `bahan_baku`
 --
 
 CREATE TABLE `bahan_baku` (
@@ -53,7 +53,7 @@ CREATE TABLE `bahan_baku` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `bahan_baku`
+-- Dumping data untuk tabel `bahan_baku`
 --
 
 INSERT INTO `bahan_baku` (`id`, `nama_bahan`, `no_part`, `stock`, `created_at`, `updated_at`) VALUES
@@ -63,7 +63,7 @@ INSERT INTO `bahan_baku` (`id`, `nama_bahan`, `no_part`, `stock`, `created_at`, 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bahan_baku_transaksi`
+-- Struktur dari tabel `bahan_baku_transaksi`
 --
 
 CREATE TABLE `bahan_baku_transaksi` (
@@ -71,6 +71,7 @@ CREATE TABLE `bahan_baku_transaksi` (
   `no_surat` varchar(255) DEFAULT NULL,
   `kode_transaksi` varchar(255) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
+  `bulan` varchar(255) DEFAULT NULL,
   `nama_bahan` varchar(255) DEFAULT NULL,
   `id_bahan` varchar(255) DEFAULT NULL,
   `no_part` varchar(255) DEFAULT NULL,
@@ -82,7 +83,25 @@ CREATE TABLE `bahan_baku_transaksi` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `capaian_produksi`
+-- Struktur dari tabel `capaian_delivery`
+--
+
+CREATE TABLE `capaian_delivery` (
+  `id` int(11) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `bulan` date DEFAULT NULL,
+  `produk` text DEFAULT NULL,
+  `no_part` varchar(255) DEFAULT NULL,
+  `plan_delivery` text DEFAULT NULL,
+  `pengiriman` int(11) DEFAULT NULL,
+  `capaian` varchar(255) DEFAULT NULL,
+  `presentase` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `capaian_produksi`
 --
 
 CREATE TABLE `capaian_produksi` (
@@ -100,12 +119,13 @@ CREATE TABLE `capaian_produksi` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pa_schedule`
+-- Struktur dari tabel `pa_schedule`
 --
 
 CREATE TABLE `pa_schedule` (
   `id` bigint(20) NOT NULL,
   `tanggal` date DEFAULT NULL,
+  `bulan` varchar(255) DEFAULT NULL,
   `produk` varchar(255) DEFAULT NULL,
   `no_part` varchar(255) DEFAULT NULL,
   `plan_produksi` int(11) DEFAULT NULL,
@@ -113,10 +133,25 @@ CREATE TABLE `pa_schedule` (
   `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data untuk tabel `pa_schedule`
+--
+
+INSERT INTO `pa_schedule` (`id`, `tanggal`, `bulan`, `produk`, `no_part`, `plan_produksi`, `created_at`, `updated_at`) VALUES
+(1, '2023-04-06', 'Januari', 'UNDER BRACKET', '1234', 123, '2023-04-06 14:07:11', '2023-04-06 14:07:11'),
+(2, '1987-10-10', 'Februari', 'UNDER BRACKET', '234xx', 0, '2023-04-07 03:44:03', '2023-04-07 03:44:03'),
+(3, '2023-04-06', 'Januari', 'UNDER BRACKET', '12345', 123, '2023-04-06 14:07:11', '2023-04-06 14:07:11'),
+(4, '2023-04-09', 'Februari', 'UNDER BRACKET', '123456', 123, '2023-04-06 14:07:11', '2023-04-06 14:07:11'),
+(5, '2023-04-09', 'Februari', 'UNDER BRACKET', '1234567', 123, '2023-04-06 14:07:11', '2023-04-06 14:07:11'),
+(6, '2023-04-09', 'Januari', 'INNER TUBE', '12345678', 123, '2023-04-06 14:07:11', '2023-04-06 14:07:11'),
+(7, '2023-04-09', 'Januari', 'INNER TUBE', '123456', 123, '2023-04-06 14:07:11', '2023-04-06 14:07:11'),
+(8, '2023-04-09', 'Februari', 'INNER TUBE', '1234567', 123, '2023-04-06 14:07:11', '2023-04-06 14:07:11'),
+(9, '2023-04-09', 'Maret', 'INNER TUBE', '12345678', 1234, '2023-04-06 14:07:11', '2023-04-06 14:07:11');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produk_jadi`
+-- Struktur dari tabel `produk_jadi`
 --
 
 CREATE TABLE `produk_jadi` (
@@ -129,7 +164,7 @@ CREATE TABLE `produk_jadi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `produk_jadi`
+-- Dumping data untuk tabel `produk_jadi`
 --
 
 INSERT INTO `produk_jadi` (`id`, `nama_produk`, `no_part`, `stock`, `created_at`, `updated_at`) VALUES
@@ -138,7 +173,27 @@ INSERT INTO `produk_jadi` (`id`, `nama_produk`, `no_part`, `stock`, `created_at`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projek`
+-- Struktur dari tabel `produk_jadi_transaksi`
+--
+
+CREATE TABLE `produk_jadi_transaksi` (
+  `id` int(11) NOT NULL,
+  `no_surat` varchar(255) DEFAULT NULL,
+  `kode_transaksi` varchar(255) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `bulan` varchar(255) DEFAULT NULL,
+  `nama_produk` varchar(255) DEFAULT NULL,
+  `id_produk` varchar(255) NOT NULL,
+  `no_part` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `projek`
 --
 
 CREATE TABLE `projek` (
@@ -153,7 +208,35 @@ CREATE TABLE `projek` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `schedule_detail`
+-- Struktur dari tabel `quality`
+--
+
+CREATE TABLE `quality` (
+  `id` int(11) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `bulan` int(11) DEFAULT NULL,
+  `shift` varchar(255) DEFAULT NULL,
+  `produk` text DEFAULT NULL,
+  `no_part` text DEFAULT NULL,
+  `hasil_produksi` text DEFAULT NULL,
+  `jenis_kecacatan` text DEFAULT NULL,
+  `kuantitas` varchar(255) DEFAULT NULL,
+  `persentase_ng` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `quality`
+--
+
+INSERT INTO `quality` (`id`, `tanggal`, `bulan`, `shift`, `produk`, `no_part`, `hasil_produksi`, `jenis_kecacatan`, `kuantitas`, `persentase_ng`, `created_at`, `updated_at`) VALUES
+(1, '1999-10-10', NULL, '1', 'UNDER BRACKET', '123-456-789', 'Hardware', 'Rusak Parah', '2', NULL, '2023-04-09 16:28:02', '2023-04-09 16:28:02');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `schedule_detail`
 --
 
 CREATE TABLE `schedule_detail` (
@@ -166,10 +249,17 @@ CREATE TABLE `schedule_detail` (
   `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data untuk tabel `schedule_detail`
+--
+
+INSERT INTO `schedule_detail` (`id`, `tanggal`, `produk`, `no_part`, `plan_produksi`, `created_at`, `updated_at`) VALUES
+(1, '2022-10-10', 'F1 Minardi', '1234', 0, '2023-04-06 22:06:00', '2023-04-06 22:06:00');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usecase`
+-- Struktur dari tabel `usecase`
 --
 
 CREATE TABLE `usecase` (
@@ -184,7 +274,7 @@ CREATE TABLE `usecase` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -203,142 +293,179 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `nama`, `email`, `password`, `is_sales`, `is_ppic`, `is_produksi`, `is_store`, `is_manajerial`, `posisi`, `create_at`, `updated_at`) VALUES
 (1, 'firja10', 'fairuzfirjatullah3@gmail.com', 'Shikamaru2209', 1, NULL, NULL, NULL, NULL, 'sales', '2023-03-05 04:38:54', '2023-03-05 04:38:54'),
-(2, 'Raja', 'dte07111840000121@gmail.com', 'Shikamaru2209', NULL, NULL, NULL, NULL, NULL, 'ppic', '2023-03-05 06:24:12', '2023-03-05 06:24:12'),
+(2, 'Raja', 'dte07111840000121@gmail.com', 'Shikamaru2209', NULL, NULL, NULL, NULL, NULL, 'ppc', '2023-03-05 06:24:12', '2023-03-05 06:24:12'),
 (3, 'storeadmin', 'fairuzf1010@gmail.com', 'Shikamaru2209', NULL, NULL, NULL, NULL, NULL, 'store', '2023-03-05 06:35:57', '2023-03-05 06:35:57'),
-(4, 'manajemen', 'fairuzf1010@gmail.com', 'Shikamaru2209', NULL, NULL, NULL, NULL, NULL, 'manajerial', '2023-03-05 07:00:06', '2023-03-05 07:00:06');
+(4, 'manajemen', 'fairuzf1010@gmail.com', 'Shikamaru2209', NULL, NULL, NULL, NULL, NULL, 'manajerial', '2023-03-05 07:00:06', '2023-03-05 07:00:06'),
+(5, 'damonhill', 'damonhill@gmail.com', 'Shikamaru2209', NULL, NULL, NULL, NULL, NULL, 'produksi', '2023-04-06 02:45:42', '2023-04-06 02:45:42');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `activity`
+-- Indeks untuk tabel `activity`
 --
 ALTER TABLE `activity`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `bahan_baku`
+-- Indeks untuk tabel `bahan_baku`
 --
 ALTER TABLE `bahan_baku`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `bahan_baku_transaksi`
+-- Indeks untuk tabel `bahan_baku_transaksi`
 --
 ALTER TABLE `bahan_baku_transaksi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `capaian_produksi`
+-- Indeks untuk tabel `capaian_delivery`
+--
+ALTER TABLE `capaian_delivery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `capaian_produksi`
 --
 ALTER TABLE `capaian_produksi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `pa_schedule`
+-- Indeks untuk tabel `pa_schedule`
 --
 ALTER TABLE `pa_schedule`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `produk_jadi`
+-- Indeks untuk tabel `produk_jadi`
 --
 ALTER TABLE `produk_jadi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `projek`
+-- Indeks untuk tabel `produk_jadi_transaksi`
+--
+ALTER TABLE `produk_jadi_transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `projek`
 --
 ALTER TABLE `projek`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `schedule_detail`
+-- Indeks untuk tabel `quality`
+--
+ALTER TABLE `quality`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `schedule_detail`
 --
 ALTER TABLE `schedule_detail`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `usecase`
+-- Indeks untuk tabel `usecase`
 --
 ALTER TABLE `usecase`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `activity`
+-- AUTO_INCREMENT untuk tabel `activity`
 --
 ALTER TABLE `activity`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `bahan_baku`
+-- AUTO_INCREMENT untuk tabel `bahan_baku`
 --
 ALTER TABLE `bahan_baku`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `bahan_baku_transaksi`
+-- AUTO_INCREMENT untuk tabel `bahan_baku_transaksi`
 --
 ALTER TABLE `bahan_baku_transaksi`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `capaian_produksi`
+-- AUTO_INCREMENT untuk tabel `capaian_delivery`
+--
+ALTER TABLE `capaian_delivery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `capaian_produksi`
 --
 ALTER TABLE `capaian_produksi`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pa_schedule`
+-- AUTO_INCREMENT untuk tabel `pa_schedule`
 --
 ALTER TABLE `pa_schedule`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `produk_jadi`
+-- AUTO_INCREMENT untuk tabel `produk_jadi`
 --
 ALTER TABLE `produk_jadi`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `projek`
+-- AUTO_INCREMENT untuk tabel `produk_jadi_transaksi`
+--
+ALTER TABLE `produk_jadi_transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `projek`
 --
 ALTER TABLE `projek`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `schedule_detail`
+-- AUTO_INCREMENT untuk tabel `quality`
 --
-ALTER TABLE `schedule_detail`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `quality`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `usecase`
+-- AUTO_INCREMENT untuk tabel `schedule_detail`
+--
+ALTER TABLE `schedule_detail`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `usecase`
 --
 ALTER TABLE `usecase`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
